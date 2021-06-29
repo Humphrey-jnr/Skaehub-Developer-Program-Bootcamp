2054bdf51d263d6d2297c68668984eb7a6bb35cc
Ben Hamphrey Nyangiye <65704925+Huvana@users.noreply.github.com>
2021-06-29 21:30:32 +0300

    Add files via upload

---
+++ DAY 6/leap_year_test.py
@@ -0,0 +1,11 @@
+import unittest
+from leap_year import *
+
+class test_leap(unittest.TestCase):
+    def test_word(self):
+        self.assertEqual((year),2002, "should be 2002")
+        
+    
+    
+if __name__=="__main__":
+    unittest.main()
